e586070d758a3574a41398d61a2ef7787cbc3c1c
Charles Heflin <charlesheflin@gmail.com>
2025-08-13 08:22:55 -0400

    Redirect Auth pageName to /auth for Supabase login

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
-
-
-
 export function createPageUrl(pageName: string) {
+    if (pageName === "Auth") {
+        return "/auth"; // route to our Supabase login page
+    }
     return '/' + pageName.toLowerCase().replace(/ /g, '-');
 }
