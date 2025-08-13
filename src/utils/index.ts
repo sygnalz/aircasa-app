6ebabae90fa3ec182461a61b600ce0a4c8b77faf
Charles Heflin <charlesheflin@gmail.com>
2025-08-13 10:58:57 -0400

    cleanup: remove Auth special-case from createPageUrl

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,3 @@
 export function createPageUrl(pageName: string) {
-    if (pageName === "Auth") {
-        return "/auth"; // route to our Supabase login page
-    }
-    return '/' + pageName.toLowerCase().replace(/ /g, '-');
+  return '/' + pageName.toLowerCase().replace(/ /g, '-');
 }
